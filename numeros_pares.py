4e4cc65ba537bbd77cfd3971a5917ce0c7de517f
BLH4CK3R <castroconttreras@gmail.com>
2024-08-25 23:01:53 -0500

    Update numeros_pares.py

--- numeros_pares.py
+++ numeros_pares.py
@@ -1,20 +1,20 @@
 print(f"\nNumeros pares\n")
 
 while True:
-    try:
-	numero = int(input("Digite un numero entero: "))
-	if numero < 0:
-	    print("[!]Error, solo se aceptan números positivos. ")
-	    continue
-	break
-    except ValueError:
-	print("[!]Error, solo se aceptan números enteros.")
+  try:
+    numero = int(input("Digite un numero entero: "))
+    if numero < 0:
+      print("[!]Error, solo se aceptan números positivos. ")
+      continue
+    break
+  except ValueError:
+    print("[!]Error, solo se aceptan números enteros.")
 
 def es_par(numero):
-    return numero%2==0
+  return numero % 2 == 0
 
 if es_par(numero):
-    print(f"{numero} es par")
+  print(f"{numero} es par")
 
 else:
-    print(f"{numero} es impar")
+  print(f"{numero} es impar")
