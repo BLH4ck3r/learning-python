697ac21d106d2b45a2115f6abce3521c62ec2332
BLH4CK3R <castroconttreras@gmail.com>
2024-08-26 10:56:10 -0500

    Update main.py

--- calculadoraOperaciones/main.py
+++ calculadoraOperaciones/main.py
@@ -1,11 +1,12 @@
-from calculos import Calculadora
-import os
+# main.py
+from calculos import Calculadora #importamos la clase Calculadora 
+import os #importamos libreria os
 
-mi_calculadora = Calculadora()
+mi_calculadora = Calculadora() #asignamos la clase Calculadora a la variable mi_calculadora
 
 while True:
-    os.system("clear")
-    print(f"\n--------Calculadora--------")
+    os.system("clear")#limpiamos pantalla
+    print(f"\n--------Calculadora--------\n")#imprimimos el menú
     print("1) Suma")
     print("2) Resta")
     print("3) Multiplicacion")
@@ -13,17 +14,17 @@ while True:
     print("5) Salir")
     print("---------------------------")
 
-    try:
+    try:#estructura de control
         opcion = int(input("Digite un opción: "))
-        if opcion < 1 or opcion > 5:
-            continue
+        if opcion < 1 or opcion > 5:#rango de opciones
+            continue#continua hasta q se digite una opción correcta
     except ValueError:
         print("Error: Ingresa un número entero válido.")
         continue
     if opcion == 5:
         print("Saliendo...")
-        break
-    while True:
+        break#sale del ciclo
+    while True:#ciclo 
         try:
             num1 = float(input("Digite el primer numero: "))
             num2 = float(input("Digite el segundo numero:"))
@@ -32,14 +33,15 @@ while True:
             print("[!]Error, solo se aceptan numeros enteros.")
 
     if opcion == 1:
-        print(mi_calculadora.suma(num1, num2))
+        print(mi_calculadora.suma(num1, num2))#llama a la funcion suma de la clase Calculadora
         break
     elif opcion == 2:
-        print(mi_calculadora.resta(num1, num2))
-        break
+        print(mi_calculadora.resta(num1, num2))#llama a la funcion resta de la clase Calculadora
+        break#termina el ciclo
     elif opcion == 3:
-        print(mi_calculadora.multiplicacion(num1, num2))
+        print(mi_calculadora.multiplicacion(num1, num2))#llama a la funcion multiplicación de la clase Calculadora
         break
     elif opcion == 4:
-        print(mi_calculadora.division(num1, num2))
+        print(mi_calculadora.division(num1, num2))#llama a la funcion división de la clase Calculadora
         break
+
