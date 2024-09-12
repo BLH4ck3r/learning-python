eca3c8fa052c6dc004f3e6b48112b657d244be52
D1ckH4ck3r <castroconttreras@gmail.com>
2024-09-12 18:01:33 -0500

    Create basicas.py

---
+++ CaculadoraMadre/Operaciones/basicas.py
@@ -0,0 +1,18 @@
+
+def sumar_numeros(num1, num2):
+    return num1 + num2
+
+def restar_numeros(num1, num2):
+    return num1 - num2
+
+def multiplicar_numeros(num1, num2):
+    return num1 * num2
+
+def dividir_numeros(num1, num2):
+    return num1 / num2
+
+def potencia_de_un_numero(num1, num2):
+    return num1 ** num2
+
+def raiaz_de_un_numero(num1, num2):
+    return num1 ** (1/num2)
