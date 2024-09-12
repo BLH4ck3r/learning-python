acece302c66a2f5f1503f9ac81d5aa0233e61a74
D1ckH4ck3r <castroconttreras@gmail.com>
2024-09-12 18:03:01 -0500

    Create matriz.py

---
+++ CaculadoraMadre/Operaciones/matriz.py
@@ -0,0 +1,31 @@
+from Utilidades.matriz import validar_operaciones, validar_multiplicacion
+
+def sumar_matriz(matriza, matrizb):
+    from Clases.matriz import Matriz
+    if validar_operaciones(matriza, matrizb):
+        resultado = Matriz(matriza, matrizb)
+        for i in range(matriza.filas):
+            for j in range(matriza.columnas):
+                resultado.matriz[i][j] = matriza.matriz[i][j] + matrizb.matriz[i][j]
+        return resultado
+
+def restar_matriz(matriza, matrizb):
+    from Clases.matriz import Matriz
+    if validar_operaciones(matriza, matrizb):
+        resultado = Matriz(matriza, matrizb)
+        for i in range(matriza.filas):
+            for j in range(matriza.columnas):
+                resultado.matriz[i][j] = matriza.matriz[i][j] - matrizb.matriz[i][j]
+        return resultado
+
+def multiplicar_matriz(matriza, matrizb):
+    from Clases.matriz import Matriz
+    if validar_multiplicacion(matriza, matrizb):
+        resultado = Matriz(matriza.filas, matrizb.columnas)
+        for i in range(matriza.filas):
+            for j in range(matrizb.columnas):
+                suma = 0
+                for k in range(matriza.columnas):
+                    suma += matriza.matriz[i][k] * matrizb.matriz[k][j]
+                resultado.matriz[i][j] = suma
+        return resultado
