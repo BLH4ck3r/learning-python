49a990a713758a23e5ecde9f87419f351cc57928
D1ckH4ck3r <castroconttreras@gmail.com>
2024-10-02 19:38:31 -0500

    Update matriz.py

--- CaculadoraMadre/Operaciones/matriz.py
+++ CaculadoraMadre/Operaciones/matriz.py
@@ -3,7 +3,7 @@ from Utilidades.matriz import validar_operaciones, validar_multiplicacion
 def sumar_matriz(matriza, matrizb):
     from Clases.matriz import Matriz
     if validar_operaciones(matriza, matrizb):
-        resultado = Matriz(matriza, matrizb)
+        resultado = Matriz(matriza.filas, matrizb.columnas)
         for i in range(matriza.filas):
             for j in range(matriza.columnas):
                 resultado.matriz[i][j] = matriza.matriz[i][j] + matrizb.matriz[i][j]
@@ -12,7 +12,7 @@ def sumar_matriz(matriza, matrizb):
 def restar_matriz(matriza, matrizb):
     from Clases.matriz import Matriz
     if validar_operaciones(matriza, matrizb):
-        resultado = Matriz(matriza, matrizb)
+        resultado = Matriz(matriza.filas, matrizb.columnas)
         for i in range(matriza.filas):
             for j in range(matriza.columnas):
                 resultado.matriz[i][j] = matriza.matriz[i][j] - matrizb.matriz[i][j]
